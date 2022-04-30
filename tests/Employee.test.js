3385080f5e2f01ee3cb9572e36d9d7bb02ca8040
Rafael Herrera <rafaelherrera731@gmail.com>
2022-04-29 22:15:44 -0700

    employee test for get() functions

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -27,3 +27,23 @@ test("Creates email", () => {
     expect(employee.email).toBe(employeeEmail)
 });
 
+//test to get name via getName()
+test("getName() functions actually retrives name", () => {
+    const testName = "Rafael";
+    const employee = new Employee(testName);
+    expect(employee.getName()).toBe(testName);
+});
+
+//test to get id via getId()
+test("getId() functions actually retrives ID", () => {
+    const testId = 10;
+    const employee = new Employee(testId);
+    expect(employee.getId()).toBe(testId);
+});
+
+//test to get email via getEmail()
+test("getEmail() functions actually retrives Email", () => {
+    const testEmail = "employeeEmail";
+    const employee = new Employee(testEmail);
+    expect(employee.getEmail()).toBe(testEmail);
+});
