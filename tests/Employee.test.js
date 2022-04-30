4c55b8ef8c626561c69544684891259466f6c57e
Rafael Herrera <rafaelherrera731@gmail.com>
2022-04-29 22:07:31 -0700

    employee test for object, name, id and email

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -6,4 +6,24 @@ test('Makes an employee object', () =>{
     expect(typeof(employee)).toBe("object");
 });
 
+//value for employee name
+test("Creates the name", () => {
+    const employeeName = "Rafael";
+    const employee = new Employee(employeeName);
+    expect(employee.employeeName).toBe(employeeName);
+});
+
+//value for ID
+test("Creates an ID", () => {
+    const idValue = 10;
+    const employee = new Employee(idValue);
+    expect(employee.id).toBe(idValue);
+});
+
+//value for email
+test("Creates email", () => {
+    const employeeEmail = "eployeeEmail";
+    const employee = new Employee(email);
+    expect(employee.email).toBe(employeeEmail)
+});
 
