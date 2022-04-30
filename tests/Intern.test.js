2b44123eebb235e1c9d37435b1678c1020d7e632
Rafael Herrera <rafaelherrera731@gmail.com>
2022-04-29 23:48:16 -0700

    test files for intern and manager

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,1 +1,21 @@
 const Intern = require ("../lib/Intern");
+
+//test for school
+test("selecting school", () => {
+    const testSchool = "school";
+    const employee = new Intern("X", 1, "testEmail", testSchool);
+    expect(employee.school).toBe(testSchool);
+});
+
+//test for getRole() and getting school
+test("getting the school name", () => {
+    const testSchool = "school";
+    const employee = new Intern("X", 1, "testEmail", "school");
+    expect(employee.getSchool()).toBe(testSchool);
+})
+
+test("getRole() to return Intern", () => {
+    const internRole = "intern";
+    const employee = new Intern("X", 1, "testEmail", "school");
+    expect(employee.getRole().toBe(internRole));
+});
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,1 +1,21 @@
 const Manager = require ("../lib/Manager");
+
+//creates contact number 
+test("creates contact number", () => {
+    const testContact = 5551234;
+    const employee = new Manager("X", 1, "testEmail", testContact);
+    expect(employee.officeNumber).toBe(testContact);
+});
+
+//test getRole() and getting contact number
+test("gets the contact number", () => {
+    const testContact = 5551234;
+    const employee = new Manager("X", 1, "testEmail", testContact);
+    expect(employee.getOfficeNumber()).toBe(testContact);
+});
+
+test("gets role using getRole()", () => {
+    const testManager = "Manager";
+    const employee = new Manager("X", 1, "testEmail", 5551234);
+    expect(employee.getRole()).toBe(testManager);
+});
